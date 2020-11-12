d87c67060818076098e31b8e9ae832da489774f3
Roman <recska.roman@gmail.com>
2020-11-12 21:05:37 +0100

    advanced json parsing implemented

--- Monster.cpp
+++ Monster.cpp
@@ -2,7 +2,7 @@
 
 void Monster::ToString()
 {
-     cout << GetName() << " HP: " << GetHp() << " DMG: " << GetDmg() << endl;
+    std::cout << GetName() << " HP: " << GetHp() << " DMG: " << GetDmg() << " ATTACKSPEED: " << attackspeed << std::endl;
 }
 
 
@@ -10,51 +10,16 @@ void Monster::SetHp(int newhp)
 {
     this->hp = newhp;
 }
-Monster Monster::parseUnit(const std::string& fileName)
+
+
+Monster Monster::parseUnit(JSON& obj)
 {
-    std::ifstream file;
-    std::string line;
-    std::string name;
-    std::string _hp;
-    std::string _dmg;
-    int hp = 0;
-    int dmg = 0;
-    int key = 0;
-    file.open(fileName);
 
-    if (!file.good()) throw 404;
-    if (file.is_open())
-    {
-        while (!file.eof())
-        {
-            getline(file, line);
-            if (key == 1)
-            {
-                size_t findcolon = line.find(":");
-                for (int i = findcolon+3; line[i] != '"'; i++)
-                {
-                     name += line[i];
-                }
-
-            }
-            if (key == 2)
-            {
-                for (unsigned int i = 0; i < line.size(); i++)
-                    if (isdigit(line[i]))
-                        _hp += line[i];
-            }
-            if (key == 3)
-            {
-                for (unsigned int i = 0; i < line.size(); i++)
-                    if (isdigit(line[i]))
-                        _dmg += line[i];
-            }
-
-            key++;
-        }
-        hp = stoi(_hp);
-        dmg = stoi(_dmg);
-        file.close();
-    }
+    std::map<std::string, std::string> charData = obj.charData;
+    std::string name = charData["name"];
+    int dmg = stoi(charData["dmg"]);
+    int hp = stoi(charData["hp"]);
     return Monster(name, hp, dmg);
-}
+
+
+};
--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include "Monster.h"
 using namespace std;
 
-void Attack(Monster& monster1, Monster& monster2) // monster1 Attacks monster2
+void Attack(Monster& monster1, Monster& monster2)
 {
     monster2.SetHp(monster2.GetHp() - monster1.GetDmg());
     if (monster2.GetHp() < 0) { monster2.SetHp(0); }
@@ -13,34 +13,39 @@ void Battle(Monster& monster1, Monster& monster2)
     while (monster1.GetHp() > 0 && monster2.GetHp() > 0)
     {
         if ((n % 2 == 0) && monster1.GetHp() > 0)
-            Attack(monster1,monster2);
+            Attack(monster1, monster2);
         else if (monster2.GetHp() > 0)
             Attack(monster2, monster1);
-
-
         n++;
 
     }
-    if (monster1.GetHp() == 0) {std::cout << monster2.GetName() << " wins Remaining HP: "<< monster2.GetHp() << endl; }
-    else {std::cout << monster1.GetName() << " wins Remaining HP: " << monster1.GetHp() << endl; }
+    if (monster1.GetHp() == 0) { std::cout << monster2.GetName() << " wins Remaining HP: " << monster2.GetHp() << endl; }
+    else { std::cout << monster1.GetName() << " wins Remaining HP: " << monster1.GetHp() << endl; }
 }
-
 
 int main(int argc, char** argv)
 {
-   if (argc == 3)
+    if (argc == 3)
         try
     {
-        Monster thor = Monster::parseUnit(argv[1]);
-        Monster hulk = Monster::parseUnit(argv[2]);
-        Battle(hulk, thor);
-    }
+        JSON json1(argv[1]);
+        JSON json2(argv[2]);
+        Monster monster1 = Monster::parseUnit(json1);
+        Monster monster2 = Monster::parseUnit(json2);
+        Battle(monster1, monster2);
 
+
+    }
     catch (int e)
     {
-        {std::cout << "File Doesnt exist" << std::endl; }
+        switch (e)
+        {
+        case MISSINGFILE:
+            std::cout << "File Doesnt exist" << std::endl;
+            break;
+        }
     }
-    if (argc < 3)
+    else
     {
         std::cout << "Wrong arguments" << std::endl;
     }
