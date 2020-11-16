6f15e0e2cd4cbb9ee3dc4979f6f25cc9cac1c50d
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-16 22:42:55 +0100

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Adventurer.json");
-  JSON json1(f);
-	Adventurer adventurer = Monster::parseUnit(json1);
-	EXPECT_EQ(adventurer.charData["name"], "Adventurer");
-	EXPECT_EQ(adventurer.charData["dmg"], "300");
-	EXPECT_EQ(adventurer.charData["hp"], "15");
-  EXPECT_EQ(adventurer.charData["cooldown"], "2");
+        JSON json1(f);
+	EXPECT_EQ(json.charData["name"], "Adventurer");
+	EXPECT_EQ(json.charData["dmg"], "300");
+	EXPECT_EQ(json.charData["hp"], "15");
+        EXPECT_EQ(json.charData["cooldown"], "2");
 }
