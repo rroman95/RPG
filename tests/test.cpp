3b3670eaafe08ddd2850c1d85535fe2c8671d656
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-16 22:59:32 +0100

    fixed wrong checking

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,7 +9,7 @@ TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Adventurer.json");
         JSON json(f);
 	EXPECT_EQ(json.charData["name"], "Adventurer");
-	EXPECT_EQ(json.charData["dmg"], "300");
-	EXPECT_EQ(json.charData["hp"], "15");
+	EXPECT_EQ(json.charData["dmg"], "15");
+	EXPECT_EQ(json.charData["hp"], "300");
         EXPECT_EQ(json.charData["cooldown"], "2");
 }
