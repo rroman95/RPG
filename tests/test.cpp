1e6f018df529d048efc028e9346864f053529720
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-19 20:24:25 +0100

    mea culpa

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,8 +9,8 @@ TEST(JSON_test, normal_json_file) {
 	std::ifstream f("Adventurer.json");
         JSON json(f);
 	EXPECT_EQ(json.charData["name"], "Adventurer");
-	EXPECT_EQ(json.charData["dmg"], "15");
 	EXPECT_EQ(json.charData["hp"], "300");
+	EXPECT_EQ(json.charData["dmg"], "15");
         EXPECT_EQ(json.charData["cooldown"], "2");
 }
 
@@ -18,8 +18,8 @@ TEST(JSON_test, whitespace_offset) {
 	std::ifstream f("Monster.json");
         JSON json(f);
 	EXPECT_EQ(json.charData["name"], "Monster");
-	EXPECT_EQ(json.charData["dmg"], "100");
-	EXPECT_EQ(json.charData["hp"], "30");
+	EXPECT_EQ(json.charData["hp"], "100");
+	EXPECT_EQ(json.charData["dmg"], "30");
         EXPECT_EQ(json.charData["cooldown"], "2.5");
 }
 
